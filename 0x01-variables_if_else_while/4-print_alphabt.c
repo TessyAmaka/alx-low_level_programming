c5e65f0600cca663c063aba77b99782ade42ffb5
TessyAmaka <theresaonwudinjoo@gmailcom>
2022-09-10 15:45:31 -0700

    task 3 - task 6

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+	if (alphabet == 'q')
+		continue;
+
+	else if (alphabet == 'e')
+		continue;
+
+	putchar (alphabet);
+}
+
+puchar ('\n');
+
+return (0);
+}
