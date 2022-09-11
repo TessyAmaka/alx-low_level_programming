90dedd078b6bd32774509e9b13c08b74adc290ee
TessyAmaka <theresaonwudinjoo@gmailcom>
2022-09-11 02:31:25 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,31 +4,24 @@
 
 /**
  *  main - Entry point
- *
- *  Return: Always 0 (Success/correct)
+ *  Return: Always (0) Success
  */
-
 int main(viod)
 {
 int n;
-
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
 if (n == 0)
 {
-printf("%i is zero\n", n);
+printf("%d is zero\n", n);
 }
-
-else if (n < 0)
+else if (n > 0)
 {
-printf("%i is negative\n", n);
+printf("%d is positive\n", n);
 }
-
 else
 {
-printf("%i is positive\n", n);
+printf("%d is nagative\n", n);
 }
-
 return (0);
 }
